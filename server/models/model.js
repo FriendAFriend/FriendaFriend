97341036b9b6ecb98af7a7b0b1665d6b0cab0fae
coralfussman <coralfussman@gmail.com>
2021-06-21 19:39:39 -0400

    merging server changes

--- server/models/model.js
+++ server/models/model.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 
 // create a new pool here using the connection string above
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DB_URI,
 });
 
 // log the query params to the console and execute the query
 module.exports = {
-    query: (text, params, callback) => {
-      console.log('executed query', text);
-      return pool.query(text, params, callback);
-    }
-  };
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
