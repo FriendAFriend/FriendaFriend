365313884309dd3b338e16554051eac65f4f0326
coralfussman <coralfussman@gmail.com>
2021-06-21 20:18:07 -0400

    added Enviornment conditional for weback in server

--- server/server.js
+++ server/server.js
@@ -7,22 +7,27 @@ const apiRouter = require('./routes/api');
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === 'production') {
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, './index.html'));
+  });
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+}
 
 // API ROUTER logic handled in api.js
 app.use('/api', apiRouter);
-
-// static router from the build folder
-app.use('/build', express.static(path.join(__dirname, '../build')));
-
-// base route serves index.html
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, './index.html'));
-});
 
 // catch-all route handler
 app.use((req, res) =>
   res.status(404).send("Sorry! I could not find the page you're looking for!")
 );
+
+app.get('*', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, './index.html'))
+);
+
+// statically serve everything in the build folder on the route '/build'
 
 // global error handler
 app.use((err, req, res, next) => {
@@ -36,6 +41,6 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
