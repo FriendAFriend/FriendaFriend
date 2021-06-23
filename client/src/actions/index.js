bf6b777e05d69ff44f13ce9d236556268e40d540
Kellen Levy <kellenlevy@kellens-mbp-2.myfiosgateway.com>
2021-06-22 20:35:36 -0400

    fixed fetch request in actionCreators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import store from '../store';
 
 export const createUser = (user) => async (dispatch, getState) => {
-  const res = await fetch('POST', 'http://localhost:8080/api', user);
+  const res = await fetch('http://localhost:8080/api', user);
   const registeredUser = await res.json();
   dispatch({ type: 'CREATE_USER', payload: registeredUser });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
+  entry: ["babel-polyfill", './client/src/index.js' ],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
