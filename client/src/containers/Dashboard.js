f56d2c999528994e406d519648dc97b1bd7d403a
Kellen Levy <kellenlevy@kellens-mbp-2.myfiosgateway.com>
2021-06-24 15:09:54 -0400

    new branch

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CreateListing from '../components/CreateListing';
+import Trips from '../components/Trips'
 function Dashboard() {
   return (
     <div>
       <CreateListing />
       <div>
         <h2>UPCOMING TRIP</h2>
+        <Trips/>
       </div>
       <div>
         <h2>VISITING GUESTS</h2>
