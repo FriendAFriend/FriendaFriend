db4bb32bfb6335d3e92abcbf312346180a0f21d7
coralfussman <coralfussman@gmail.com>
2021-06-24 16:25:34 -0400

    search bar working again in dev

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,7 +14,6 @@ export const createUser = (user) => async (dispatch, getState) => {
   dispatch({ type: 'CREATE_USER', payload: registeredUser });
 };
 
-
 export const loginUser = (user) => async (dispatch, getState) => {
   console.log('from create action redux login', user);
   try {
@@ -27,10 +26,10 @@ export const loginUser = (user) => async (dispatch, getState) => {
     });
 
     const loggedInUser = await res.json();
-    loggedInUser.isLoggedIn = true
+    loggedInUser.isLoggedIn = true;
     //dispatch({ type: 'LOGIN_USER', payload: loggedInUser });
   } catch (err) {
-    user.isLoggedIn = true
+    user.isLoggedIn = true;
     dispatch({ type: 'LOGIN_USER', payload: user });
     //dispatch({ type: 'LOGIN_FAILURE', payload: true });
   }
@@ -48,24 +47,21 @@ export const createListing = (listing) => async (dispatch, getState) => {
   dispatch({ type: 'NEW_LISTING', payload: newListing });
 };
 
-
-
 // store.dispatch({
 //   type: 'createAccount',
 //   payload: {
 //     description: 'userInfo',
 //   },
 // });
-export const fetchListings = () => (dispatch) => {
-  fetch('http://localhost:8080/api')
-    .then((res) => res.json())
-    .then(
-      dispatch({
-        type: 'FETCH_LISTINGS',
-        payload: listing.data,
-      })
-      //(listing) => console.log(listing)
-    );
+export const fetchListings = () => async (dispatch, getState) => {
+  const res = await fetch('http://localhost:8080/api/dashboard');
+  const listing = await res.json();
+  console.log(listing, 'all listings from fetch');
+
+  dispatch({
+    type: 'FETCH_LISTINGS',
+    payload: listing,
+  });
 };
 
 export const filterListingsByName = (listings, name) => (dispatch) => {
@@ -94,10 +90,10 @@ export const filterListingsByCity = (listings, location) => (dispatch) => {
 };
 
 export const getTrips = (user) => async (dispatch, getState) => {
-  const res = await fetch('http://localhost:8080/user/userTrips')
-  const userTrips = await res.json()
-  dispatch({ type: 'GET_USER_TRIPS', payload: userTrips})
-}
+  const res = await fetch('http://localhost:8080/user/userTrips');
+  const userTrips = await res.json();
+  dispatch({ type: 'GET_USER_TRIPS', payload: userTrips });
+};
 
 // ! photo upload actions, experimental
 
@@ -105,7 +101,7 @@ export const addPhotos = (photosArray) => {
   return (dispatch) => {
     dispatch({
       type: 'addPhotos',
-      payload: photosArray
+      payload: photosArray,
     });
   };
 };
@@ -113,7 +109,7 @@ export const addPhotos = (photosArray) => {
 export const clearPhotos = () => {
   return (dispatch) => {
     dispatch({
-      type: 'clearPhotos'
+      type: 'clearPhotos',
     });
   };
 };
@@ -122,7 +118,7 @@ export const uploadingSwitch = (bool) => {
   return (dispatch) => {
     dispatch({
       type: 'uploadingSwitch',
-      payload: bool
+      payload: bool,
     });
   };
 };
