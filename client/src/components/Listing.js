5bfc6d32fba44afc0a412004fe4f4d3871d6eca8
coralfussman <coralfussman@gmail.com>
2021-06-23 21:16:39 -0400

    fetch to all listings working and rendering to components

--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function Listing() {
+function Listing(props) {
+  // const {name} = props
   return (
     <div>
       <h2>New Listing</h2>
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,40 +1,36 @@
-/* this router will handle API calls to handle listings, photos and pets */ 
+/* this router will handle API calls to handle listings, photos and pets */
 
-const express = require('express'); 
+const express = require('express');
 const listingController = require('../controllers/listingController');
 const photoController = require('../controllers/photoController');
 const petController = require('../controllers/petController');
-const router = express.Router(); 
+const router = express.Router();
 
 // default for dashboard view should display all listings (pagination will come later)
-router.get('/dashboard', 
-  listingController.getAllListings, 
-  (req, res) => {
-    res.status(200).json(res.locals.listings);
-  }
-);
+router.get('/dashboard', listingController.getAllListings, (req, res) => {
+  res.status(200).json(res.locals.allListings);
+});
 
 // page for only the user's own listings
-router.get('/myListings', 
-  listingController.getListing,
-  (req, res) => {
-    res.status(200).json(res.locals.listings);
-  }
-);
+router.get('/myListings', listingController.getListing, (req, res) => {
+  res.status(200).json(res.locals.listings);
+});
 
 // post request creates a listing with associated pet and photos table insertions
-router.post('/newListing',
+router.post(
+  '/newListing',
   listingController.createListing,
-  // ! do we then want to reroute them to the dashboard and display all listings? 
+  // ! do we then want to reroute them to the dashboard and display all listings?
   (req, res) => {
     // res.redirect('/dashboard');
     res.status(200).json(res.locals.listing);
   }
 );
 
-router.post('/newPet',
+router.post(
+  '/newPet',
   listingController.createListing,
-  // ! do we then want to reroute them to the dashboard and display all listings? 
+  // ! do we then want to reroute them to the dashboard and display all listings?
   (req, res) => {
     // res.redirect('/dashboard');
     res.status(200).json(res.locals.listing);
@@ -42,23 +38,18 @@ router.post('/newPet',
 );
 
 // update an already-created listing; different from routes for deleting or adding photos
-router.put('/updateListing', 
-  listingController.updateListing,
-  (req, res) => {
-    // res.redirect('/dashboard');
-    res.status(200).json(res.locals.listing);
-  }
-);
+router.put('/updateListing', listingController.updateListing, (req, res) => {
+  // res.redirect('/dashboard');
+  res.status(200).json(res.locals.listing);
+});
 
-router.post('/newPhoto', 
-  photoController.createPhoto,
-  (req, res) => {
-    res.status(200).json(res.locals.photos);
-  }
-);
+router.post('/newPhoto', photoController.createPhoto, (req, res) => {
+  res.status(200).json(res.locals.photos);
+});
 
 // delete a listing permanently, associated photos and pet
-router.delete('/', 
+router.delete(
+  '/',
   listingController.deleteListing,
   photoController.deletePhoto,
   petController.deletePet,
